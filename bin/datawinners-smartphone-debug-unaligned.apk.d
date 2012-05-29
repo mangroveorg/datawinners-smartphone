04bd2cee8c0bbf7e96754075730d796b7d2fd336
Geet Chandratre <geetchandratre@gmail.com>
2012-05-29 11:55:01 +0530

    ant build

---
+++ bin/datawinners-smartphone-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/geet/dev/mangrove/DW-ANDROID-REPOS/datawinners-smartphone/bin/datawinners-smartphone-debug-unaligned.apk : \
+/home/geet/dev/mangrove/DW-ANDROID-REPOS/datawinners-smartphone/bin/datawinners-smartphone.ap_ \
+/home/geet/dev/mangrove/DW-ANDROID-REPOS/datawinners-smartphone/bin/classes.dex \
+/home/geet/Desktop/Softwares/android-sdk-linux/tools/support/annotations.jar \
---
+++ bin/datawinners-smartphone-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/home/geet/dev/mangrove/DW-ANDROID-REPOS/datawinners-smartphone/bin/datawinners-smartphone-release-unsigned.apk : \
+/home/geet/dev/mangrove/DW-ANDROID-REPOS/datawinners-smartphone/bin/datawinners-smartphone.ap_ \
+/home/geet/dev/mangrove/DW-ANDROID-REPOS/datawinners-smartphone/bin/classes.dex \
+/home/geet/Desktop/Softwares/android-sdk-linux/tools/support/annotations.jar \
