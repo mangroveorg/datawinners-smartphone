e5120ebe2798f498dffdd52040b667489f684843
Anirudha <anirudg@thoughtworks.com>
2012-05-31 15:23:13 +0530

    Anirudha : deleting output dirs

--- bin/datawinners-smartphone-release-unsigned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/geet/dev/mangrove/DW-ANDROID-REPOS/datawinners-smartphone/bin/datawinners-smartphone-release-unsigned.apk : \
-/home/geet/dev/mangrove/DW-ANDROID-REPOS/datawinners-smartphone/bin/datawinners-smartphone.ap_ \
-/home/geet/dev/mangrove/DW-ANDROID-REPOS/datawinners-smartphone/bin/classes.dex \
-/home/geet/Desktop/Softwares/android-sdk-linux/tools/support/annotations.jar \
