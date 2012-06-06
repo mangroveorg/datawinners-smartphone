7b78ca8cec23c4eb431524111a6f50dc08f5d890
Anirudha <anirudg@thoughtworks.com>
2012-06-06 16:06:32 +0530

    Anirudha/Geet: jar libs dir for odk collect

--- dev-setup.sh
+++ dev-setup.sh
@@ -4,6 +4,7 @@ hg clone https://code.google.com/p/opendatakit.collect/
 android update lib-project --target 2 --path ./opendatakit.collect/
 cd opendatakit.collect/
 echo "android.library=true" >> project.properties
+echo "jar.libs.dir=lib" >> ant.properties
 cd ../datawinners-smartphone/
-echo "android.library.reference.1=../opendatakit.collect" > project.properties
+echo "android.library.reference.1=../opendatakit.collect" >> project.properties
 ln -s ../opendatakit.collect/lib ./libs
