f1eefbc35bed18015dc3ffede12e681647de6b47
Anirudha <anirudg@thoughtworks.com>
2012-06-07 13:04:52 +0530

    Anirudha : Geet : odk collect in same directory, fixed dependency to version cae3c64dbea7

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,9 +1,9 @@
-cd ..
-rm -rf opendatakit.collect/ 
+rm -rf ./opendatakit.collect/
 hg clone https://code.google.com/p/opendatakit.collect/
-android update lib-project --target 2 --path ./opendatakit.collect/
-cd opendatakit.collect/
+cd ./opendatakit.collect/
+hg update cae3c64dbea7
+android update lib-project --target 2 --path ./
 echo "android.library=true" >> project.properties
 echo "jar.libs.dir=lib" >> ant.properties
-cd ../datawinners-smartphone/
-ln -s ../opendatakit.collect/lib ./libs
+cd ..
+ln -s ./opendatakit.collect/lib ./libs
