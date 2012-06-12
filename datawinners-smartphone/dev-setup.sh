fadf47235792afabb536cc4b1892b8d504f84d54
Anirudha <anirudg@thoughtworks.com>
2012-06-12 14:30:27 +0530

    Anirudha : falling back on 1.2 RC1 with date-picker patch, as Edit forms was crashing

--- datawinners-smartphone/dev-setup.sh
+++ datawinners-smartphone/dev-setup.sh
@@ -1,7 +1,8 @@
 rm -rf ./opendatakit.collect/
 hg clone https://code.google.com/p/opendatakit.collect/
 cd ./opendatakit.collect/
-hg update cae3c64dbea7
+hg update 15185704d55c
+hg import ../month-year.patch -m"issue 432  - provide a "month-year" and "year" appearance to show only those"
 android update lib-project --target 2 --path ./
 echo "android.library=true" >> project.properties
 echo "jar.libs.dir=lib" >> ant.properties
