26b5a64778c698b84a34c4515a793f0ff915d318
Geet Chandratre <geetchandratre@gmail.com>
2012-06-13 13:20:44 +0530

    Geet | Changing run automation tests script for smartphone

--- functional-tests/run-tests.sh
+++ functional-tests/run-tests.sh
@@ -1,10 +1,10 @@
+ant debug
 echo "Launching emulator "
 emulator -avd $AVD_NAME &
 echo "Waiting for emulator to initialize "
 sleep 70
 adb install -r ../datawinners-smartphone/bin/datawinners-smartphone-debug.apk
 cd ../functional-tests
-ant debug
 adb install -r ./bin/automation-debug.apk
 adb shell input keyevent 82
 echo "Running tests "
