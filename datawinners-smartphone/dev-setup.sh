7cae4e0675203fe7872ad8b17fcfa95144079de8
Anirudha <anirudg@thoughtworks.com>
2012-06-13 15:03:05 +0530

    Anirudha : changed ODK version again as datepicker was crashing in android 3.0+ version

--- datawinners-smartphone/dev-setup.sh
+++ datawinners-smartphone/dev-setup.sh
@@ -1,7 +1,7 @@
 rm -rf ./opendatakit.collect/
 hg clone https://code.google.com/p/opendatakit.collect/
 cd ./opendatakit.collect/
-hg update 15185704d55c
+hg update f3d6d92d3066
 hg import ../month-year.patch -m"issue 432  - provide a "month-year" and "year" appearance to show only those"
 android update lib-project --target 2 --path ./
 echo "android.library=true" >> project.properties
