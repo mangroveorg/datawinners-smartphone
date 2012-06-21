7fc61ad43efc22cd61e46a36a91eb1e1aa08694d
Geet Chandratre <geetchandratre@gmail.com>
2012-06-21 14:55:19 +0530

    Geet | Adding script for installing android sdk with required versions

---
+++ README.rst
@@ -0,0 +1,20 @@
+Setting Up the development environment
+======================================
+
+* Clone the repository:
+
+* Install latest ant
+
+    sudo apt-get install ant
+
+* Install mercurial
+	
+	sudo apt-get install mercurial
+
+* cd datawinners-smartphone/datawinners-smartphone
+
+* Run the following script 
+	
+    . sdk-setup.sh (This might take a lot of time as it downloads the android SDK on your machine.)
+
+	. dev-setup.sh
---
+++ datawinners-smartphone/sdk-setup.sh
@@ -0,0 +1,14 @@
+cd ../../
+wget http://dl.google.com/android/android-sdk_r18-linux.tgz
+tar xvzf android-sdk_r18-linux.tgz
+cd android-sdk-linux
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'SDK Platform Android 4.0.3, API 15, revision 3' | cut -c 4-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Platform-tools, revision 11' | cut -c 4-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Tools, revision 19' | cut -c 4-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'ARM EABI v7a System Image, Android API 15, revision 2' | cut -c 3-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Google APIs, Android API 15, revision 2' | cut -c 3-4`
+echo 'export PATH=/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:`pwd`/platform-tools:`pwd`/tools' >> ~/.bashrc
+echo 'export ANDROID_HOME=`pwd`' >> ~/.bashrc
+. ~/.bashrc
+
+
