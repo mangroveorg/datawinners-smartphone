1f281dad795797c975310888c86c18e1dffcf8d0
Geet Chandratre <geetchandratre@gmail.com>
2012-06-29 11:19:58 +0530

    Geet, Ashwini | Changing dev setup

--- datawinners-smartphone/dev-setup.sh
+++ datawinners-smartphone/dev-setup.sh
@@ -5,6 +5,6 @@ hg update f3d6d92d3066
 hg import ../month-year.patch -m"issue 432  - provide a "month-year" and "year" appearance to show only those"
 android update lib-project --target 2 --path ./
 echo "android.library=true" >> project.properties
-echo "jar.libs.dir=lib" >> ant.properties
+ln -s ./lib ./libs
 cd ..
 ln -s ./opendatakit.collect/lib ./libs
