9f828471fe543c3e170206d394296a82ce2a7aae
Geet Chandratre <geetchandratre@gmail.com>
2012-07-06 16:57:30 +0530

    Geet , Argha | Adding patch for installation of both apps (ODK, Datawinners)

--- datawinners-smartphone/dev-setup.sh
+++ datawinners-smartphone/dev-setup.sh
@@ -2,7 +2,7 @@ rm -rf ./opendatakit.collect/
 hg clone https://code.google.com/p/opendatakit.collect/
 cd ./opendatakit.collect/
 hg update f3d6d92d3066
-hg import ../month-year.patch -m"issue 432  - provide a "month-year" and "year" appearance to show only those"
+hg import --no-commit ../month-year.patch ../authorities.patch
 android update lib-project --target 2 --path ./
 echo "android.library=true" >> project.properties
 ln -s ./lib ./libs
