c4055cc7840bcc3c150d4b76e5dda8211f0700b0
lakshmirp <rp.lakshmi.45@gmail.com>
2013-03-22 11:36:35 +0530

    anishek/ashwin | changing the versions of android tools and platform as the older versions are not accessible anymore

--- datawinners-smartphone/sdk-setup.sh
+++ datawinners-smartphone/sdk-setup.sh
@@ -1,10 +1,10 @@
 cd ../../
-wget http://dl.google.com/android/android-sdk_r18-linux.tgz
-tar xvzf android-sdk_r18-linux.tgz
+wget http://dl.google.com/android/android-sdk_r21-linux.tgz
+tar xvzf android-sdk_r21-linux.tgz
 cd android-sdk-linux
 ./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'SDK Platform Android 4.0.3, API 15, revision 3' | cut -c 4-4`
-./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Platform-tools, revision 11' | cut -c 4-4`
-./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Tools, revision 19' | cut -c 4-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Platform-tools, revision 16.0.2' | cut -c 4-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Tools, revision 21.1' | cut -c 4-4`
 ./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'ARM EABI v7a System Image, Android API 15, revision 2' | cut -c 3-4`
 ./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Google APIs, Android API 15, revision 2' | cut -c 3-4`
 echo 'export PATH=/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:`pwd`/platform-tools:`pwd`/tools' >> ~/.bashrc
