613bb259e1b7492094c4534fe03aac73ac5c95d5
Deepthi Chand <deepthia@thoughtworks.com>
2014-08-22 14:11:32 +0530

    DC/Pooja/Melanie | #3788 | upgrading odk version to 1.4

--- datawinners-smartphone/dev-setup.sh
+++ datawinners-smartphone/dev-setup.sh
@@ -1,8 +1,8 @@
 rm -rf ./opendatakit.collect/
 hg clone https://code.google.com/p/opendatakit.collect/
 cd ./opendatakit.collect/
-hg update f3d6d92d3066
-hg import --no-commit ../month-year.patch ../authorities.patch
+hg update b00ca8d82040
+hg import --no-commit ../authorities.patch ../icons.patch
 android update lib-project --target 2 --path ./
 echo "android.library=true" >> project.properties
 ln -s ./lib ./libs
--- datawinners-smartphone/sdk-setup.sh
+++ datawinners-smartphone/sdk-setup.sh
@@ -1,14 +1,14 @@
 cd ../../
-wget http://dl.google.com/android/android-sdk_r21-linux.tgz
-tar xvzf android-sdk_r21-linux.tgz
+wget http://dl.google.com/android/android-sdk_r23-linux.tgz
+tar xvzf android-sdk_r23-linux.tgz
 cd android-sdk-linux
-./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'SDK Platform Android 4.0.3, API 15, revision 3' | cut -c 3-4`
-./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Platform-tools, revision 16.0.2' | cut -c 3-4`
-./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Tools, revision 21.1' | cut -c 3-4`
-./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'ARM EABI v7a System Image, Android API 15, revision 2' | cut -c 3-4`
-./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Google APIs, Android API 15, revision 2' | cut -c 3-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'SDK Platform Android 4.4.2, API 19, revision 3' | cut -c 3-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Platform-tools, revision 20' | cut -c 3-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Tools, revision 23.0.2' | cut -c 3-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Google APIs (ARM System Image), Android API 19, revision 7' | cut -c 3-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Google APIs, Android API 19, revision 2' | cut -c 3-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Google Play services, revision 19' | cut -c 3-4`
+
 echo 'export PATH=/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:`pwd`/platform-tools:`pwd`/tools' >> ~/.bashrc
 echo 'export ANDROID_HOME=`pwd`' >> ~/.bashrc
 . ~/.bashrc
-
-
