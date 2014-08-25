70a97b59b897ae6e010da1593301ae35a430ab1a
Deepthi Chand <deepthia@thoughtworks.com>
2014-08-25 12:23:36 +0530

    DC/Pooja | #3788 | modifying script to setup the app from commandline

--- datawinners-smartphone/dev-setup.sh
+++ datawinners-smartphone/dev-setup.sh
@@ -2,9 +2,10 @@ rm -rf ./opendatakit.collect/
 hg clone https://code.google.com/p/opendatakit.collect/
 cd ./opendatakit.collect/
 hg update b00ca8d82040
-hg import --no-commit ../authorities.patch ../icons.patch
+hg import --no-commit ../authorities.patch ../icons.patch ../target_package_change.patch
 android update lib-project --target 2 --path ./
 echo "android.library=true" >> project.properties
-ln -s ./lib ./libs
 cd ..
-ln -s ./opendatakit.collect/lib ./libs
+mkdir libs
+cp -r opendatakit.collect/libs/* libs/
+cp -r playservices/libs/* libs/
--- datawinners-smartphone/sdk-setup.sh
+++ datawinners-smartphone/sdk-setup.sh
@@ -7,8 +7,16 @@ cd android-sdk-linux
 ./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Tools, revision 23.0.2' | cut -c 3-4`
 ./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Google APIs (ARM System Image), Android API 19, revision 7' | cut -c 3-4`
 ./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Google APIs, Android API 19, revision 2' | cut -c 3-4`
+./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Android SDK Build-tools, revision 20' | cut -c 3-4`
 ./tools/android update sdk --no-ui --filter `./tools/android list sdk | grep 'Google Play services, revision 19' | cut -c 3-4`
+cp -r extras/google/google_play_services/libproject/google-play-services_lib/ ../datawinners-smartphone/datawinners-smartphone/playservices
 
 echo 'export PATH=/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:`pwd`/platform-tools:`pwd`/tools' >> ~/.bashrc
 echo 'export ANDROID_HOME=`pwd`' >> ~/.bashrc
 . ~/.bashrc
+
+sudo apt-get install ﻿lib32stdc++6
+﻿sudo apt-get install lib32z1
+cp ../datawinners-smartphone/datawinners-smartphone/build-playservices.xml ../datawinners-smartphone/datawinners-smartphone/playservices/build.xml
+cd tools
+android update project --name datawinners-smartphone --target 3 --path ../../datawinners-smartphone/datawinners-smartphone --subprojects
