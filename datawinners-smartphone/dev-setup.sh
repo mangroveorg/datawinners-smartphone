8aa9a57b169caf54c5a12512794c5b47a831d051
Ajay Divakaran <ajay@dwdev.ajay>
2015-01-29 00:45:25 +0530

    Ajay | Added patch to remove odk batching when media files present

--- datawinners-smartphone/dev-setup.sh
+++ datawinners-smartphone/dev-setup.sh
@@ -2,9 +2,10 @@ rm -rf ./opendatakit.collect/
 hg clone https://code.google.com/p/opendatakit.collect/
 cd ./opendatakit.collect/
 hg update 4dba16025944
-hg import --no-commit ../authorities.patch ../icons.patch ../target_package_change.patch
+hg import --no-commit ../authorities.patch ../icons.patch ../target_package_change.patch ../removed-send-batching.patch
 android update lib-project --target 2 --path ./
 echo "android.library=true" >> project.properties
 cd ..
+rm -rf libs/
 cp -rf opendatakit.collect/libs/ .
 cp -r playservices/libs/* libs/
