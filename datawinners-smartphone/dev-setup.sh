72bbd0162b6ca46c6417b5f8c65114988f798b6e
natchu <222natchu93@gmail.com>
2016-04-04 07:29:53 +0530

    Natchu | #4981 | Updated dev-setup.sh with new patches

--- datawinners-smartphone/dev-setup.sh
+++ datawinners-smartphone/dev-setup.sh
@@ -2,7 +2,7 @@ rm -rf ./opendatakit.collect/
 hg clone https://code.google.com/p/opendatakit.collect/
 cd ./opendatakit.collect/
 hg update 4dba16025944
-hg import --no-commit ../authorities.patch ../icons.patch ../target_package_change.patch ../removed-send-batching.patch ../enable_autocomplete.patch ../fix_backspace_bug_in_autocomplete.patch ../warning_message_toast_for_autocomplete.patch
+hg import --no-commit ../authorities.patch ../icons.patch ../target_package_change.patch ../removed-send-batching.patch ../enable_autocomplete.patch ../fix_backspace_bug_in_autocomplete.patch ../warning_message_toast_for_autocomplete.patch ../download_external_dataset.patch
 android update lib-project --target 2 --path ./
 echo "android.library=true" >> project.properties
 cd ..
